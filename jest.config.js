3b59994ab8e66676378341b4ca9f3b76c51a975c
johnrusch <johnrusch7@gmail.com>
2025-07-15 17:40:40 -0700

    fix: Update Jest config to handle Expo module resolution
    - Simplified Jest configuration
    - Added expo-.* and @fortawesome to transformIgnorePatterns
    - Removed testEnvironment override to use jest-expo defaults
    - Added clearMocks and resetMocks for test isolation
    
    Note: Tests may still fail due to Expo/RN compatibility issues that
    will be addressed in follow-up work

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|aws-amplify|@aws-amplify|react-native-vector-icons)'
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|aws-amplify|@aws-amplify|react-native-vector-icons|expo-.*|@fortawesome)'
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js'
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
@@ -24,8 +22,6 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/amplify/', '/amplify-backup/'],
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  globals: {
-    __DEV__: true
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node']
+  clearMocks: true,
+  resetMocks: true
 };
